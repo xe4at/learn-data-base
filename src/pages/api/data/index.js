b99b103d1a7235461c493701d6072810e17d77cc
Ehsan Atashkar <ehsanwebdev84@gmail.com>
2025-03-28 18:14:33 +0330

    this project just for learn database and writing api in next

---
+++ src/pages/api/data/index.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { name } = req.body;
+
+    if (!name || name.length <= 3) {
+      res.status(422).json({ status: "failed", message: "Invalid Data" });
+      return;
+    }
+
+    res.status(201).json({
+      status: "success",
+      message: "Data Created",
+      data: { name },
+    });
+  }
+}
